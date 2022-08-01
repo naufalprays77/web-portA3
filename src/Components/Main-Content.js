985c40af00627a199af9358f17dcdc2ef2f73395
nanda.prayasdewo <nanda.prayasdewo@gmail.com>
2022-08-01 16:50:36 +0700

    add url main-content

--- src/Components/Main-Content.js
+++ src/Components/Main-Content.js
@@ -7,7 +7,7 @@ import ToDoList from '../Img/ToDoList.png';
 export const CalculatorSec = () => {
   return (
     <div className=" md:px-6 md:py-3 px-4 py-1">
-      <Link to="/Calculator ">
+      <Link to="/web-portA3/Calculator ">
         <img src={Calculator} alt="calculator" className="md:w-[100px] w-[300px] hover:scale-150 duration-1000 hover:duration-500 " />
       </Link>
     </div>
@@ -17,7 +17,7 @@ export const CalculatorSec = () => {
 export const API = () => {
   return (
     <div className=" md:px-6 md:py-3 px-6 py-1">
-      <Link to="/API">
+      <Link to="/web-portA3/API">
         <img src={APIIcon} alt="API" className="md:w-[100px] w-[300px] hover:scale-150 duration-1000 hover:duration-500" />
       </Link>
     </div>
@@ -27,7 +27,7 @@ export const API = () => {
 export const Todo = () => {
   return (
     <div className=" md:px-6 md:py-3 px-4 py-1">
-      <Link to="/ToDoList">
+      <Link to="/web-portA3/ToDoList">
         <img src={ToDoList} alt="ToDoList" className="md:w-[100px] w-[300px] hover:scale-150 duration-1000 hover:duration-500" />
       </Link>
     </div>
